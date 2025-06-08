ff27eb5687213f6ecb4c5601884c1b87b50ce1d4
V A DINESH <dineshva2004@gmail.com>
2025-06-08 15:32:45 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -6,7 +6,7 @@ import jwt_decode from 'jwt-decode';
 //https://project-management-app-production-3d51.up.railway.app/api/
 //http://localhost:8700/api/
 //https://vexa.onrender.com/
-const API = axios.create({ baseURL: `http://localhost:8800/api` }); 
+const API = axios.create({ baseURL: `https://projectra.onrender.com/api` }); 
 
 
 
